acd16f1467d63cd5499c419ead06e46472ab2354
chrisiv123 <31861185+chrisiv123@users.noreply.github.com>
2019-10-04 08:59:39 -0400

    Update EchoClient.java

--- EchoClient.java
+++ EchoClient.java
@@ -1,3 +1,4 @@
+// by christian ivano 100555278, Assignment 1 for distributed systems
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,14 +9,16 @@ import java.util.Scanner;
 public class EchoClient {
 
     public static void main(String[] args) {
-
+// host is local host and port used is 3500.
         String host = "localhost";
         int port = 3500;
+	    // connecting to the server.
         try (Socket socket = new Socket(host, port)) {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Scanner scanner = new Scanner(System.in);
             String line = null;
+		// the game starts.
             System.out.println("You have connected to a Bulls and cows game.");
             System.out.println("The point of this game is to find the secret number before the others");
             System.out.println("Just enter a 4 digit number and you will get feedback on how close");
@@ -25,13 +28,14 @@ public class EchoClient {
             System.out.println("");
             System.out.println("Now good luck and go get them!");
             System.out.println(" if you want to exit just say so :)");
+		// constantly asking for inputs.
             while (!"exit".equalsIgnoreCase(line)) {
 				System.out.println(" That was not the correct answer, try something else or exit to leave");
                 line = scanner.nextLine();
                 out.println(line);
                 out.flush();
                 System.out.println(in.readLine());
-            }
+            }// closeing all the opened resources.
             scanner.close();
         } catch (IOException e) {
             e.printStackTrace();
